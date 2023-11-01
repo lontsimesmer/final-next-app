25931a7a691455dea83ba69867919839a32be79c
mesmer <tchoupounathanael@gmail.com>
2023-11-01 13:26:25 +0100

    add landing page design

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,96 @@
-import Image from 'next/image'
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={150}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="font-mono">
+      <div className="bg-indigo-500 flex h-20 items-center justify-between px-56">
+        <p className="font-bold text-white text-3xl">reef.</p>
+        <div className="flex gap-8">
+          <button className="h-10 rounded-md text-white w-24">Sign up</button>
+          <button className="bg-purple-600 h-10 hover:bg-purple-700 rounded-md text-white w-48">
+            Request a demo
+          </button>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={40}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
+      <div className="flex gap-24 justify-center mx-auto my-24 w-3/4">
+        <div className="w-1/2">
+          <h1 className="font-black leading-snug pb-3 text-indigo-900 text-5xl tracking-tight">
+            Plug into a better way to manage international teams
+          </h1>
+          <img
+            className="h-auto max-w-xl rounded-xl"
+            src="https://assets-global.website-files.com/6050f2ad3687cc808784a212/623a8ab1426dcf979ccc8d8c_grow.jpeg"
+            alt="image description"
+          />
+          <p className="font-medium pt-6 text-indigo-900 tracking-tighter">
+            Tap into better team management tools with plugins for PTO, Org
+            Charts, and much more to build a stronger company culture, increase
+            team collaboration, and automate more processes. Give employees the
+            tools to connect their growth to competencies so their progression
+            turns into business impact.
           </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        </div>
+        <div className="bg-gray-50 max-w-screen-sm h-4/5 p-8 rounded-2xl w-full">
+          <form className="bg-white px-10 pt-12 pb-8 rounded-lg shadow-2xl">
+            <div className="mb-4">
+              <h2 className="font-bold mb-5 text-indigo-900 text-3xl tracking-tighter">
+                Request a Demo
+              </h2>
+              <label
+                className="block font-bold leading-none mb-1 text-indigo-900 text-sm tracking-tight"
+                htmlFor="firstname"
+              >
+                <small>First Name:</small>
+              </label>
+              <input
+                className="border border-gray-300 h-10 leading-tight py-2 px-3 rounded w-full text-indigo-900"
+                id="firstname"
+                type="text"
+              />
+            </div>
+            <div className="mb-6">
+              <label
+                className="block font-bold leading-none mb-1 text-indigo-900 text-sm tracking-tight"
+                htmlFor="lastname"
+              >
+                <small>Last Name:</small>
+              </label>
+              <input
+                className="border border-gray-300 h-10 py-2 px-3 rounded w-full text-indigo-900 mb-3 leading-tight"
+                id="lastname"
+                type="text"
+              />
+              <label
+                className="block font-bold leading-none mb-1 text-indigo-900 text-sm"
+                htmlFor="username"
+              >
+                <small>Email Address:</small>
+              </label>
+              <input
+                className="border border-gray-300 py-2 px-3 rounded w-full text-indigo-900 leading-tight"
+                id="username"
+                type="text"
+              />
+            </div>
+            <div className="flex items-center justify-between">
+              <button
+                className="bg-blue-500 mt-8 font-bold hover:bg-blue-700 px-4 py-3 rounded text-white w-full"
+                type="button"
+              >
+                Get Started
+              </button>
+            </div>
+            <p className="font-sans leading-none mt-3 text-left text-gray-900 text-xs">
+              <small>
+                This site is protected by reCAPTCHA and the Google Privacy
+                Policy and Terms of Service apply. By submitting your
+                information, you agree to Lattice's Terms of Service and Privacy
+                Policy. You can opt out anytime.
+              </small>
+            </p>
+          </form>
+        </div>
       </div>
     </main>
-  )
+  );
 }
