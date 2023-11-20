e1e42ba9eba907631f8023eb9050d2093fcdfc31
lontsimesmer <tchoupounathanael@gmail.com>
2023-11-20 16:54:21 +0100

    add new style to section

--- app/page.tsx
+++ app/page.tsx
@@ -2,27 +2,27 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="font-Silka">
-      <div className="bg-indigo-500 fixed flex h-20 items-center justify-between px-56 top-0 w-full z-20">
+    <main className="font-Matter">
+      <div className="bg-indigo-500 fixed flex h-20 items-center justify-between px-60 top-0 w-full z-20">
         <p className="font-bold text-white text-3xl">reef.</p>
-        <div className="flex gap-8">
+        <div className="flex gap-2">
           <button className="h-10 rounded-md text-white w-24">Sign up</button>
-          <button className="bg-purple-600 h-10 hover:bg-purple-700 rounded-md text-white w-48">
+          <button className="bg-purple-600 h-10 hover:bg-purple-700 rounded-lg text-white w-40">
             Request a demo
           </button>
         </div>
       </div>
-      <div className="flex gap-24 justify-center mx-auto mb-20 mt-40 w-3/4">
+      <div className="flex gap-20 justify-center mx-auto mb-20 mt-40 w-3/4">
         <div className="w-1/2">
-          <h1 className="font-black leading-snug pb-3 text-indigo-900 text-5xl tracking-tight">
-            Plug into a better way to manage international teams
+          <h1 className="font-black leading-snug pb-3 text-indigo-900 text-5xl tracking-wide">
+            Plug into a better way to manage international <br /> teams
           </h1>
           <img
             className="h-auto max-w-xl rounded-xl"
             src="https://assets-global.website-files.com/6050f2ad3687cc808784a212/623a8ab1426dcf979ccc8d8c_grow.jpeg"
             alt="image description"
           />
-          <p className="font-medium pt-6 text-indigo-900 tracking-tight">
+          <p className="font-medium pt-6 text-lg text-indigo-900 tracking-tight">
             Tap into better team management tools with plugins for PTO, Org
             Charts, and much more to build a stronger company culture, increase
             team collaboration, and automate more processes. Give employees the
@@ -33,7 +33,7 @@ export default function Home() {
         <div className="bg-gray-50 max-w-screen-sm h-4/5 p-8 rounded-2xl w-full">
           <form className="bg-white px-10 pt-12 pb-8 rounded-lg shadow-2xl">
             <div>
-              <h2 className="font-bold mb-5 text-indigo-900 text-3xl tracking-tighter">
+              <h2 className="font-bold mb-5 text-indigo-900 text-3xl tracking-tight">
                 Request a Demo
               </h2>
               <label
@@ -92,19 +92,19 @@ export default function Home() {
         </div>
       </div>
       <section className="bg-indigo-50">
-        <div className="flex gap-24 justify-center mx-auto py-24 w-3/4">
-          <div className="w-1/2">
-            <h1 className="font-black leading-snug text-indigo-900 text-5xl tracking-tight">
+        <div className="flex gap-20 items-center justify-center mx-auto py-32 w-3/4">
+          <div className="flex flex-col gap-6 w-2/5">
+            <h1 className="font-black leading-snug text-indigo-900 text-5xl tracking-wide">
               Reef's the only team management platform with everything you need,
               for everyone
             </h1>
-            <p className="font-medium text-gray-900 tracking-tight">
+            <p className="font-normal text-lg text-gray-900 tracking-normal">
               Reef Engage is designed to keep your team connected and engaged,
               regardless of location. With a 92% adoption rate within the first
               week, our platform integrates seamlessly with Slack to ensure
               efficient communication, engagement monitoring, and more.
             </p>
-            <button className="bg-purple-600 h-10 hover:bg-purple-700 rounded-md text-white w-40">
+            <button className="bg-purple-600 h-10 hover:bg-purple-700 rounded-lg text-white w-36">
               Book a demo
             </button>
           </div>
@@ -112,134 +112,139 @@ export default function Home() {
             className="h-auto max-w-xl rounded-xl"
             src="https://19498232.fs1.hubspotusercontent-na1.net/hubfs/19498232/OOO.jpg"
             alt="image description"
+            style={{ height: 600 }}
           />
         </div>
       </section>
-      <section className="flex flex-col gap-10 justify-center mx-auto my-24 w-3/4">
-        <div className="flex flex-col items-center justify-center text-center">
-          <h1 className="font-black leading-snug text-indigo-900 text-5xl tracking-tight">
+      <section className="flex flex-col gap-20 justify-center mx-auto my-24 w-3/4">
+        <div className="flex flex-col gap-3 items-center justify-center text-center">
+          <h1 className="font-black leading-snug text-indigo-900 text-5xl tracking-wide">
             Some clear reasons to choose <br /> Reef to seamlessly manage your{" "}
             <br /> team
           </h1>
-          <p className="font-medium text-gray-900 tracking-tight">
+          <p className="font-normal text-lg text-gray-900 tracking-normal">
             Our guaranteed top-tier service and industry-leading worldwide
             coverage <br /> provide peace of mind for any global team. With
             Reef's next-level automation <br /> and in-house infrastructure,
             quality always comes first for our customers.
           </p>
-          <button className="bg-purple-600 h-10 hover:bg-purple-700 rounded-md text-white w-40">
+          <button className="bg-purple-600 h-10 hover:bg-purple-700 mt-6 rounded-lg text-white w-36">
             Book a demo
           </button>
         </div>
-        <div className="grid grid-cols-3 gap-4">
-          <div className="flex flex-col items-center text-center">
-            <h2 className="font-black leading-snug text-indigo-900 text-5xl tracking-tight">
+        <div className="grid grid-cols-3 gap-20">
+          <div className="flex flex-col gap-3 items-center text-center">
+            <h2 className="font-black leading-snug text-indigo-900 text-5xl tracking-wide">
               1.25min
             </h2>
-            <p className="font-medium text-gray-900 tracking-tight">
+            <p className="font-light text-lg text-gray-900 tracking-normal">
               First response time with 24/7 in <br />
               -app support
             </p>
           </div>
-          <div className="flex flex-col items-center text-center">
-            <h2 className="font-black leading-snug text-indigo-900 text-5xl tracking-tight">
+          <div className="flex flex-col gap-3 items-center text-center">
+            <h2 className="font-black leading-snug text-indigo-900 text-5xl tracking-wide">
               200+
             </h2>
-            <p className="font-medium text-gray-900 tracking-tight">
+            <p className="font-light text-lg text-gray-900 tracking-normal">
               In-house tax, payroll and legal <br /> specialists
             </p>
           </div>
-          <div className="flex flex-col items-center text-center">
-            <h2 className="font-black leading-snug text-indigo-900 text-5xl tracking-tight">
+          <div className="flex flex-col gap-3 items-center text-center">
+            <h2 className="font-black leading-snug text-indigo-900 text-5xl tracking-wide">
               80+hrs
             </h2>
-            <p className="font-medium text-gray-900 tracking-tight">
+            <p className="font-light text-lg text-gray-900 tracking-normal">
               Saved by customers monthly <br /> automating contractor <br />{" "}
               payments
             </p>
           </div>
-          <div className="flex flex-col items-center text-center">
-            <h2 className="font-black leading-snug text-indigo-900 text-5xl tracking-tight">
+          <div className="flex flex-col gap-3 items-center text-center">
+            <h2 className="font-black leading-snug text-indigo-900 text-5xl tracking-wide">
               20+
             </h2>
-            <p className="font-medium text-gray-900 tracking-tight">
+            <p className="font-light text-lg text-gray-900 tracking-normal">
               Integrations to seamlessly sync <br /> with your current tech
               stack
             </p>
           </div>
-          <div className="flex flex-col items-center text-center">
-            <h2 className="font-black leading-snug text-indigo-900 text-5xl tracking-tight">
+          <div className="flex flex-col gap-3 items-center text-center">
+            <h2 className="font-black leading-snug text-indigo-900 text-5xl tracking-wide">
               15+
             </h2>
-            <p className="font-medium text-gray-900 tracking-tight">
+            <p className="font-light text-lg text-gray-900 tracking-normal">
               Flexible payment methods to <br /> pay your team
             </p>
           </div>
-          <div className="flex flex-col items-center text-center">
-            <h2 className="font-black leading-snug text-indigo-900 text-5xl tracking-tight">
+          <div className="flex flex-col gap-3 items-center text-center">
+            <h2 className="font-black leading-snug text-indigo-900 text-5xl tracking-wide">
               20,000+
             </h2>
-            <p className="font-medium text-gray-900 tracking-tight">
+            <p className="font-light text-lg text-gray-900 tracking-normal">
               Deel customers, from small <br /> startups to household names
             </p>
           </div>
         </div>
       </section>
-      <section className="bg-indigo-50">
-        <div className="flex gap-24 justify-center mx-auto py-24 w-3/4">
+      <section className="bg-indigo-50 flex flex-col gap-32 py-32">
+        <div className="flex gap-20 items-center justify-center mx-auto w-3/4">
           <img
             className="h-auto max-w-xl rounded-xl"
             src="https://19498232.fs1.hubspotusercontent-na1.net/hubfs/19498232/one-platform-1.jpg"
             alt="image description"
           />
-          <div className="w-1/2">
-            <h1 className="font-black leading-snug text-indigo-900 text-5xl tracking-tight">
+          <div className="flex flex-col gap-10">
+            <h1 className="font-black leading-snug text-indigo-900 text-5xl tracking-wide">
               Enjoy the benefits of <br /> having everything in one <br />{" "}
               single platform
             </h1>
-            <p className="font-medium text-gray-900 tracking-tight">
-              Save on your tech stack with one HR platform
-            </p>
-            <p className="font-medium text-gray-900 tracking-tight">
-              Reduce onboarding time to just 5 minutes
-            </p>
-            <p className="font-medium text-gray-900 tracking-tight">
-              Eliminate errors and duplicate work with one process
-            </p>
-            <p className="font-medium text-gray-900 tracking-tight">
-              Get a holistic view of employer costs, headcount and churn
-            </p>
-            <p className="font-medium text-gray-900 tracking-tight">
-              Give your whole team a consistent experience
-            </p>
-            <button className="bg-purple-600 h-10 hover:bg-purple-700 rounded-md text-white w-40">
-              Book a demo
+            <div className="flex flex-col gap-2">
+              <p className="font-normal text-lg text-gray-900 tracking-normal">
+                Save on your tech stack with one HR platform
+              </p>
+              <p className="font-normal text-lg text-gray-900 tracking-normal">
+                Reduce onboarding time to just 5 minutes
+              </p>
+              <p className="font-normal text-lg text-gray-900 tracking-normal">
+                Eliminate errors and duplicate work with one process
+              </p>
+              <p className="font-normal text-lg text-gray-900 tracking-normal">
+                Get a holistic view of employer costs, headcount and churn
+              </p>
+              <p className="font-normal text-lg text-gray-900 tracking-normal">
+                Give your whole team a consistent experience
+              </p>
+            </div>
+            <button className="bg-purple-600 h-10 hover:bg-purple-700 rounded-lg text-white w-32">
+              Learn more
             </button>
           </div>
         </div>
-        <div className="flex gap-24 justify-center mx-auto py-24 w-3/4">
-          <div className="w-1/2">
-            <h1 className="font-black leading-snug text-indigo-900 text-5xl tracking-tight">
+        <div className="flex gap-20 items-center justify-center mx-auto w-3/4">
+          <div className="flex flex-col gap-10">
+            <h1 className="font-black leading-snug text-indigo-900 text-5xl tracking-wide">
               Run compliant payroll <br /> for your global team in <br />{" "}
               minutes
             </h1>
-            <p className="font-medium text-gray-900 tracking-tight">
-              Bulk pay contractors, EORs, and direct employees effortlessly
-            </p>
-            <p className="font-medium text-gray-900 tracking-tight">
-              We handle taxes, provide benefits, payslips, and way more
-            </p>
-            <p className="font-medium text-gray-900 tracking-tight">
-              Eliminate admin with automated contractor invoicing
-            </p>
-            <p className="font-medium text-gray-900 tracking-tight">
-              Give your team flexibility with 15+ global payment options
-            </p>
-            <p className="font-medium text-gray-900 tracking-tight">
-              Local, legal, tax, and accounting expertise just a click away
-            </p>
-            <button className="bg-purple-600 h-10 hover:bg-purple-700 rounded-md text-white w-40">
-              Book a demo
+            <div className="flex flex-col gap-2">
+              <p className="font-normal text-lg text-gray-900 tracking-normal">
+                Bulk pay contractors, EORs, and direct employees effortlessly
+              </p>
+              <p className="font-normal text-lg text-gray-900 tracking-normal">
+                We handle taxes, provide benefits, payslips, and way more
+              </p>
+              <p className="font-normal text-lg text-gray-900 tracking-normal">
+                Eliminate admin with automated contractor invoicing
+              </p>
+              <p className="font-normal text-lg text-gray-900 tracking-normal">
+                Give your team flexibility with 15+ global payment options
+              </p>
+              <p className="font-normal text-lg text-gray-900 tracking-normal">
+                Local, legal, tax, and accounting expertise just a click away
+              </p>
+            </div>
+            <button className="bg-purple-600 h-10 hover:bg-purple-700 rounded-lg text-white w-32">
+              Learn more
             </button>
           </div>
           <img
@@ -250,8 +255,8 @@ export default function Home() {
         </div>
       </section>
       <section className="flex flex-col gap-24 items-center justify-center my-24">
-        <h1 className="font-black leading-snug text-indigo-900 text-5xl tracking-tight">
-          This is why 1000s of customers love Reef
+        <h1 className="font-black leading-snug text-indigo-900 text-5xl text-center tracking-wide">
+          This is why 1000s of customers <br /> love Reef
         </h1>
         <div className="flex gap-5">
           <div
@@ -932,7 +937,7 @@ export default function Home() {
                   </li>
                   <li>
                     <a href="#" className="hover:underline">
-                      I love Human Newsletter
+                      I ❤️ Human Newsletter
                     </a>
                   </li>
                   <li className="flex gap-3 items-center">
